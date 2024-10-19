272116c1f410d83627a81090fa33593cd881e65b
Yisus550 <jesus.alfonso2402@gmail.com>
2024-10-18 22:06:35 -0700

    Se agrego modelo de Producto

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -9,12 +9,12 @@ namespace proy_caguamanta.Models
 		public int Id { get; set; }
 
 		[Required(ErrorMessage = "Este campo es requerido.")]
-		[StringLength(30)]
+		[StringLength(30, ErrorMessage = "La cadena de texto no puede sobrepasar los 30 caracteres")]
 		[Display(Name = "Nombre del producto")]
 		public string Nombre { get; set; }
 
 		[Required(ErrorMessage = "Este campo es requerido.")]
-		[StringLength(100)]
+		[StringLength(80, ErrorMessage = "La cadena de texto no puede sobrepasar los 80 caracteres")]
 		[Display(Name = "Descripción del producto")]
 		public string Descripcion { get; set; }
 
