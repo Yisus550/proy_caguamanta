58bd113a3f7b6da8c2388b8e493e0f7653d91e8e
Yisus550 <jesus.alfonso2402@gmail.com>
2024-10-18 22:06:44 -0700

    Se agrego modelo de Proveedor

---
+++ Models/Proveedor.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace caguamanta2._0.Models;
+
+public partial class Proveedor
+{
+    [Key]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int IdProveedor { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[StringLength(60, ErrorMessage = "La cadena de texto no puede sobrepasar los 60 caracteres")]
+	[Display(Name = "Nombre del proveedor")]
+	public string Nombre { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[StringLength(60, ErrorMessage = "La cadena de texto no puede sobrepasar los 60 caracteres")]
+	[Display(Name = "Apellido del proveedor")]
+	public string Apellido { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+    [Phone]
+	public string Telefono { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+    [EmailAddress]
+	public string Correo { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+    [StringLength(80)]
+	[Display(Name = "Empresa")]
+	public string Empresa { get; set; }
+}
