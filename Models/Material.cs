fd4177163029c1df78d70896c2131bc113044edd
Yisus550 <jesus.alfonso2402@gmail.com>
2024-10-18 22:06:28 -0700

    Se agrego modelo de Material

---
+++ Models/Material.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace caguamanta2._0.Models;
+
+public partial class Material
+{
+    [Key]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int IdMaterial { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[StringLength(60, ErrorMessage = "La cadena de texto no puede sobrepasar los 60 caracteres")]
+	[Display(Name = "Nombre del material")]
+	public string Nombre { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[Range(1, int.MaxValue, ErrorMessage = "La cantidad no debe de ser menor a 1")]
+	[Display(Name = "Cantidad")]
+	public int Cantidad { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[StringLength(60, ErrorMessage = "La cadena de texto no puede sobrepasar los 60 caracteres")]
+	[Display(Name = "Proveedor")]
+	public string Proveedor { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[Range(1, double.MaxValue, ErrorMessage = "El costo no debe de ser menor a $1")]
+	[Display(Name = "Costo")]
+	public decimal Costo { get; set; }
+}
