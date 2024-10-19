5fd6279e69fed55326a83a3ff512f9f7c232b83d
Yisus550 <jesus.alfonso2402@gmail.com>
2024-10-18 22:05:20 -0700

    Se agrego modelo de Compra

---
+++ Models/Compra.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace caguamanta2._0.Models;
+
+public partial class Compra
+{
+    [Key]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int Id { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[DataType(DataType.Date)]
+	public DateOnly FechaCompra { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int IdEmpleado { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int IdProveedor { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	[Range(1, double.MaxValue, ErrorMessage = "El importe no debe de ser menor a $1")]
+    public double Importe { get; set; }
+}
