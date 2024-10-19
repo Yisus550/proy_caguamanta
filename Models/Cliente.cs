55a6bff4df13b6210db0a5c383bd6cb53fdae274
Yisus550 <jesus.alfonso2402@gmail.com>
2024-10-18 22:05:08 -0700

    Se agrego modelo de Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace caguamanta2._0.Models;
+
+public partial class Cliente
+{
+    [Key]
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int IdCliente { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [StringLength(60, ErrorMessage = "La cadena de texto no puede sobrepasar los 60 caracteres")]\
+	[Display(Name = "Nombre")]
+	public string Nombre { get; set; }
+
+	[Required(ErrorMessage = "Este campo es requerido.")]
+	[DataType(DataType.PhoneNumber)]
+	[Display(Name = "Teléfono")]
+	public string Telefono { get; set; }
+}
