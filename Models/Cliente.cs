120193a0ae9664f9011e3bf0354d74d156fa1bb2
Yisus550 <jesus.alfonso2402@gmail.com>
2024-11-03 19:23:56 -0700

    Se agregaron Tests clases para los modelos de Categoria, Cliente y Compra, tambien se modificaron estos para que los unit test se puedan ejecutar correctamente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,16 +5,16 @@ namespace proy_caguamanta.Models;
 public partial class Cliente
 {
     [Key]
-	[Required(ErrorMessage = "Este campo es obligatorio")]
-	public int Id { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public int? Id { get; set; }
 
     [Required(ErrorMessage = "Este campo es obligatorio")]
     [StringLength(60, ErrorMessage = "La cadena de texto no puede sobrepasar los 60 caracteres")]
-	[Display(Name = "Nombre")]
-	public string Nombre { get; set; }
+    [Display(Name = "Nombre")]
+    public string Nombre { get; set; }
 
-	[Required(ErrorMessage = "Este campo es requerido.")]
-	[DataType(DataType.PhoneNumber)]
-	[Display(Name = "Teléfono")]
-	public string Telefono { get; set; }
+    [Required(ErrorMessage = "Este campo es requerido.")]
+    [Phone(ErrorMessage = "El formato debe de ser de un número de celular")]
+    [Display(Name = "Teléfono")]
+    public string Telefono { get; set; }
 }
--- Models/Compra.cs
+++ Models/Compra.cs
@@ -5,20 +5,20 @@ namespace proy_caguamanta.Models;
 public partial class Compra
 {
     [Key]
-	[Required(ErrorMessage = "Este campo es obligatorio")]
-	public int Id { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public int? Id { get; set; }
 
-	[Required(ErrorMessage = "Este campo es obligatorio")]
-	[DataType(DataType.Date)]
-	public DateOnly FechaCompra { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [DataType(DataType.Date)]
+    public DateOnly? FechaCompra { get; set; }
 
-	[Required(ErrorMessage = "Este campo es obligatorio")]
-	public int IdEmpleado { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public int? IdEmpleado { get; set; }
 
-	[Required(ErrorMessage = "Este campo es obligatorio")]
-	public int IdProveedor { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public int? IdProveedor { get; set; }
 
-	[Required(ErrorMessage = "Este campo es obligatorio")]
-	[Range(1, double.MaxValue, ErrorMessage = "El importe no debe de ser menor a $1")]
-    public double Importe { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [Range(1, double.MaxValue, ErrorMessage = "El importe no debe de ser menor a $1")]
+    public double? Importe { get; set; }
 }
