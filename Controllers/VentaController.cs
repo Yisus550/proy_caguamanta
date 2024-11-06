bf18038e129e7f5ecb3cc1d5dd6256983b1fcfba
Yisus550 <jesus.alfonso2402@gmail.com>
2024-11-06 11:30:13 -0700

    Se agrego logica para lista de elementos de productos y total de para la boleta

--- Controllers/VentaController.cs
+++ Controllers/VentaController.cs
@@ -1,50 +1,92 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using proy_caguamanta.Data;
 using proy_caguamanta.Models;
 
 namespace proy_caguamanta.Controllers
 {
-	public class VentaController : Controller
-	{
-		public readonly ApplicationDbContext _context;
+    public class VentaController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        private static List<Object> _productos;
 
-		public VentaController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        private static int cantidad;
+        private static double precio;
 
-		public ActionResult Index()
-		{
-			return View();
-		}
+        public VentaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-		public IActionResult Listar()
-		{
-			List<Venta> listaVenta = _context.Ventas.ToList();
-			return View(listaVenta);
-		}
+        public ActionResult Index()
+        {
+            List<SelectListItem> options = _context.Productos.Select(p => new SelectListItem
+            {
+                Text = p.Nombre,
+                Value = p.Id.ToString()
+            }).ToList();
 
-		// sobrecaragr el metodo
-		[HttpGet]
-		public IActionResult Crear()
-		{
-			return View();
-		}
-		[HttpPost]
-		public IActionResult Crear(Venta venta)
-		{
-			//validar
-			if (ModelState.IsValid)
-			{
-				// agregar, guardar y redireccionar
-				_context.Ventas.Add(venta);
-				_context.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			else
-			{
-				return View("Crear", venta);
-			}
-		}
-	}
+            ViewBag.SelectListProductos = options;
+            ViewBag.Productos = _productos;
+            ViewBag.Total = _productos != null ? _productos.Sum(p => (double)p.GetType().GetProperty("SubTotal").GetValue(p)) : 0;
+
+            return View();
+        }
+
+        public IActionResult Listar()
+        {
+            List<Venta> listaVenta = _context.Ventas.ToList();
+            return View(listaVenta);
+        }
+
+        // sobrecaragr el metodo
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(Venta venta)
+        {
+            //validar
+            if (ModelState.IsValid)
+            {
+                // agregar, guardar y redireccionar
+                _context.Ventas.Add(venta);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Crear", venta);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AgregarProducto(int productoId, int cantidadProducto)
+        {
+            Producto producto = _context.Productos.Find(productoId);
+
+            if (_productos == null)
+            {
+                _productos = new List<Object>();
+            }
+            _productos.Add(new
+            {
+                producto.Id,
+                producto.Nombre,
+                producto.Precio,
+                cantidadProducto,
+                SubTotal = producto.Precio * cantidadProducto
+            });
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult LimpiarTabla()
+        {
+            _productos = new List<Object>();
+            return RedirectToAction("Index");
+        }
+    }
 }
