86140e28dc799da4a980f4483b734d43f1c52972
Yisus550 <jesus.alfonso2402@gmail.com>
2024-11-20 08:18:20 -0700

    Agrego pagina Listar y se empezo con pagina de Compra

--- Controllers/CompraController.cs
+++ Controllers/CompraController.cs
@@ -1,49 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using proy_caguamanta.Data;
 using proy_caguamanta.Models;
-using System.ComponentModel;
 
 namespace proy_caguamanta.Controllers
 {
-    public class CompraController : Controller
-    {
-        //Crear variable 
-        public readonly ApplicationDbContext _context;
+	public class CompraController : Controller
+	{
+		//Crear variable 
+		public readonly ApplicationDbContext _context;
 
-        //Crear constructor
-        public CompraController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		private static List<ProductosList> _productos;
+		private static List<SelectListItem> options;
+		private static List<Venta> listaVenta;
 
-        //Metodos de accion
-        public IActionResult Index() 
-        {
-            List<Compra> listaCompra = _context.Compras.ToList();
-            return View(listaCompra);
-        }
+		private int cantidad;
+		private int idEmpleado;
+		private int idCliente;
+		private double precio;
+		private double total;
+		private double cambio;
 
-        [HttpGet]
-        public IActionResult Crear()
-        {
-            return View();
-        }
+		//Crear constructor
+		public CompraController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        [HttpPost]
-        public IActionResult Crear(Compra compra)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Compras.Add(compra);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View("Crear", compra);
-            }
+		public ActionResult Index(double pago, int idCliente = 1001, int idEmpleado = 2001)
+		{
+			CargarDatos(pago, idCliente, idEmpleado);
+			ViewBag.IdVenta = _context.Ventas.OrderBy(v => v.Id).Last().Id + 1;
+			ViewBag.IdCliente = this.idCliente;
+			ViewBag.IdEmpleado = this.idEmpleado;
+			return View();
+		}
 
-        }
+		//Metodos de accion
+		public IActionResult Listar()
+		{
+			List<Compra> listaCompra = _context.Compras.ToList();
+			return View(listaCompra);
+		}
+
+		[HttpGet]
+		public IActionResult Crear()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Crear(Compra compra)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Compras.Add(compra);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View("Crear", compra);
+			}
+
+		}
 
 		[HttpGet]
 		public IActionResult Editar(int id)
@@ -77,6 +97,42 @@ namespace proy_caguamanta.Controllers
 			_context.Compras.Remove(compra);
 			_context.SaveChanges();
 			return RedirectToAction("Index");
+		}
+
+		private void CargarDatos(double pago, int idCliente = 1, int idEmpleado = 1)
+		{
+			CargarProductos();
+			CalcularTotal();
+			CalcularCambio(pago);
+			// Incrementar el 'Id' de la ultima venta para obtener el actual. No funciona con '++', se le tiene que agregar '+ 1'
+			this.idEmpleado = idEmpleado;
+			this.idCliente = idCliente;
+		}
+
+		private void CargarProductos()
+		{
+			options = _context.Productos.Select(p => new SelectListItem
+			{
+				Text = p.Nombre,
+				Value = p.Id.ToString()
+			}).ToList();
+
+			ViewBag.SelectListProductos = options;
+			ViewBag.Productos = _productos;
+		}
+
+		private void CalcularTotal()
+		{
+			total = _productos != null ? _productos.Sum(p => p.SubTotal) : 0;
+			ViewBag.Total = total;
+		}
+
+		private void CalcularCambio(double pago)
+		{
+			cambio = pago <= 0 ? 0 : pago - total;
+
+			ViewBag.Pago = pago;
+			ViewBag.Cambio = cambio >= 0 ? cambio : 0;
 		}
 	}
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,123 +6,123 @@ using System.Diagnostics;
 
 namespace proy_caguamanta.Controllers
 {
-    public class HomeController : Controller
-    {
-        public readonly ApplicationDbContext _context;
+	public class HomeController : Controller
+	{
+		public readonly ApplicationDbContext _context;
 
-        public HomeController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		public HomeController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Index(Login login)
-        {
-            Empleado empleado_encontrado = await _context.Empleados
-                                           .Where(e => e.Correo == login.Correo &&
-                                                       e.Contrasena == login.Contrasena
-                                           ).FirstOrDefaultAsync();
-            if (empleado_encontrado == null)
-            {
-                return View();
+		[HttpPost]
+		public async Task<IActionResult> Index(Login login)
+		{
+			Empleado empleado_encontrado = await _context.Empleados
+										   .Where(e => e.Correo == login.Correo &&
+													   e.Contrasena == login.Contrasena
+										   ).FirstOrDefaultAsync();
+			if (empleado_encontrado == null)
+			{
+				return View();
 
-            }
-            else
-            {
-                return RedirectToAction("Index", "Venta");
-            }
+			}
+			else
+			{
+				return RedirectToAction("Index", "Venta");
+			}
 
 
-        }
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
 
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Login(Empleado empleado)
-        {
-            //Empleado empleado_encontrado = await context.Empleados.Where(e => e.Contrasena == empleado.Contrasena)
-            return RedirectToAction("Index", "Venta");
-        }
+		[HttpPost]
+		public async Task<IActionResult> Login(Empleado empleado)
+		{
+			//Empleado empleado_encontrado = await context.Empleados.Where(e => e.Contrasena == empleado.Contrasena)
+			return RedirectToAction("Index", "Venta");
+		}
 
-        public IActionResult Registros()
-        {
-            return View();
-        }
+		public IActionResult Registros()
+		{
+			return View();
+		}
 
-        public IActionResult Categoria()
-        {
-            return RedirectToAction("Index", "Categoria");
-        }
+		public IActionResult Categoria()
+		{
+			return RedirectToAction("Index", "Categoria");
+		}
 
-        public IActionResult Cliente()
-        {
-            return RedirectToAction("Index", "Cliente");
-        }
+		public IActionResult Cliente()
+		{
+			return RedirectToAction("Index", "Cliente");
+		}
 
-        public IActionResult Compra()
-        {
-            return RedirectToAction("Index", "Compra");
-        }
+		public IActionResult Compra()
+		{
+			return RedirectToAction("Listar", "Compra");
+		}
 
-        public IActionResult DetalleCompra()
-        {
-            return RedirectToAction("Index", "DetalleCompra");
-        }
+		public IActionResult DetalleCompra()
+		{
+			return RedirectToAction("Index", "DetalleCompra");
+		}
 
-        public IActionResult DetalleVenta()
-        {
-            return RedirectToAction("Index", "DetalleVenta");
-        }
+		public IActionResult DetalleVenta()
+		{
+			return RedirectToAction("Index", "DetalleVenta");
+		}
 
-        public IActionResult Empleado()
-        {
-            return RedirectToAction("Index", "Empleado");
-        }
+		public IActionResult Empleado()
+		{
+			return RedirectToAction("Index", "Empleado");
+		}
 
-        public IActionResult Material()
-        {
-            return RedirectToAction("Index", "Material");
-        }
+		public IActionResult Material()
+		{
+			return RedirectToAction("Index", "Material");
+		}
 
-        public IActionResult Producto()
-        {
-            return RedirectToAction("Index", "Producto");
-        }
+		public IActionResult Producto()
+		{
+			return RedirectToAction("Index", "Producto");
+		}
 
-        public IActionResult Proveedor()
-        {
-            return RedirectToAction("Index", "Proveedor");
-        }
+		public IActionResult Proveedor()
+		{
+			return RedirectToAction("Index", "Proveedor");
+		}
 
-        public IActionResult Puesto()
-        {
-            return RedirectToAction("Index", "Puesto");
-        }
+		public IActionResult Puesto()
+		{
+			return RedirectToAction("Index", "Puesto");
+		}
 
-        public IActionResult Venta()
-        {
-            return RedirectToAction("Listar", "Venta");
-        }
-    }
+		public IActionResult Venta()
+		{
+			return RedirectToAction("Listar", "Venta");
+		}
+	}
 }
